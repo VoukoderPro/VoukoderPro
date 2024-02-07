d3f9306052efa6b060bd6d57bfbad3e572115090
Xu Che <chrisxuche@gmail.com>
2024-02-07 22:30:31 +0800

    Remove dead code

--- AppPlugins/DaVinciResolvePlugin/voukoderpro.hpp
+++ AppPlugins/DaVinciResolvePlugin/voukoderpro.hpp
@@ -43,39 +43,6 @@ public:
         return 0;
     }
 
-    static int voukoderpro_config(std::string& config)
-    {
-        int ret = 0;
-
-        return MessageBoxW(NULL, L"Configure me!", L"Voukoder Plugin", MB_OKCANCEL | MB_ICONEXCLAMATION);
-
-        //// Find out the home directory
-        //fs::path voukoderPro;
-        //if ((ret = GetVoukoderLocation(voukoderPro)) < 0)
-        //    return -1;
-
-        //auto path = voukoderPro.parent_path() / "VoukoderProConfig.exe /Select";
-
-        //bp::ipstream is;
-        //ret = bp::system(path.string(), bp::std_out > is);
-        //if (ret == 0)
-        //{
-        //    std::stringstream ss;
-        //    
-        //    std::string line;
-        //    while (std::getline(is, line) && !line.empty())
-        //        ss << line;
-
-        //    is.pipe().close();
-
-        //    config = ss.str();
-
-        //    return 0;
-        //}
-
-        //return ret;
-    }
-
 protected:
     boost::function<pluginapi_create_t> factory;
     boost::shared_ptr<VoukoderPro::IClient> vkdrpro = nullptr;
